08dd2a29867f67029d0eb20924856f81c1db3133
s-tefan <40999112+s-tefan@users.noreply.github.com>
2019-12-10 20:41:49 +0100

    May now extend code by changing list to dict

--- aoc_2019_intcode.py
+++ aoc_2019_intcode.py
@@ -1,5 +1,5 @@
 debug = False
-#debug = True
+debug = True
 
 
 # Implements intcode as a python class up to day 5
@@ -7,7 +7,9 @@ debug = False
 class Intcode:
 
     def interpret_parameters(self, n_params, modes):
-        inparams = self.code[self.pos+1:self.pos+1+n_params]
+        inparams = []
+        for k in range(n_params): # slice does not work if the code is a dict
+            inparams.append(self.lookup(self.pos+1+k))
         pos = [None]*n_params
         for k in range(n_params):
             mode = modes%10
@@ -16,48 +18,71 @@ class Intcode:
                 pos[k] = inparams[k]
             elif mode == 1:
                 pos[k] = self.pos+1+k
+            elif mode == 2:
+                pos[k] = inparams[k] + self.relative_base
             else:
                 pass
         return pos
+
+    def lookup(self,pos):
+        try:
+            return self.code[pos]
+        except IndexError:
+            return 0
+        except KeyError:
+            return 0
+
+    def store(self,pos,val):
+        try:
+            self.code[pos]=val
+        except IndexError:
+            # raise IndexError('Trying to store code outside list. Try init code as dict.')
+            print('Changing stored code from list to dict')
+            self.make_code_dict()
+
+    def make_code_dict(self):
+        dict_code = { i : self.code[i] for i in range(len(self.code) ) }
+        self.code = dict_code
 
 # operations for the different opcodes
 
     # 1
     def add(self, mode=0):
         par_pos = self.interpret_parameters(3,mode)
-        self.code[par_pos[2]] = \
-            self.code[par_pos[0]] + self.code[par_pos[1]]
+        self.store(par_pos[2], \
+            self.lookup(par_pos[0]) + self.lookup(par_pos[1])
+            )
         self.pos += 4
         return True
 
     # 2
     def mult(self, mode=0):
         par_pos = self.interpret_parameters(3,mode)
-        self.code[par_pos[2]] = \
-            self.code[par_pos[0]] * self.code[par_pos[1]]
+        self.store(par_pos[2], \
+            self.lookup(par_pos[0]) * self.lookup(par_pos[1])
+            )
         self.pos += 4
         return True
     
     # 3
     def input(self, mode=0):
         par_pos = self.interpret_parameters(1,mode)
-        self.code[par_pos[0]] = \
-            int(input('>'))
+        self.store(par_pos[0], int(input('>')))
         self.pos += 2
         return True
 
     # 4
     def output(self, mode=0):
         par_pos = self.interpret_parameters(1,mode)
-        print(self.code[par_pos[0]])
+        print(self.lookup(par_pos[0]))
         self.pos += 2
         return True
 
     # 5
     def jump_if_true(self,mode):
         par_pos = self.interpret_parameters(2,mode)
-        if self.code[par_pos[0]]:
-            self.pos=self.code[par_pos[1]]
+        if self.lookup(par_pos[0]):
+            self.pos=self.lookup(par_pos[1])
         else:
             self.pos += 3
         return True
@@ -65,8 +90,8 @@ class Intcode:
     # 6
     def jump_if_false(self,mode):
         par_pos = self.interpret_parameters(2,mode)
-        if not self.code[par_pos[0]]:
-            self.pos=self.code[par_pos[1]]
+        if not self.lookup(par_pos[0]):
+            self.pos=self.lookup(par_pos[1])
         else:
             self.pos += 3
         return True
@@ -74,21 +99,29 @@ class Intcode:
     # 7
     def less_than(self,mode):
         par_pos = self.interpret_parameters(3,mode)
-        if self.code[par_pos[0]] < self.code[par_pos[1]]:
-            self.code[par_pos[2]] = 1
+        if self.lookup(par_pos[0]) < self.lookup(par_pos[1]):
+            self.store(par_pos[2], 1)
         else:
-            self.code[par_pos[2]] = 0
+            self.store(par_pos[2], 0)
         self.pos += 4
         return True
 
-    # 7
+    # 8
     def equals(self,mode):
         par_pos = self.interpret_parameters(3,mode)
-        if self.code[par_pos[0]] == self.code[par_pos[1]]:
-            self.code[par_pos[2]] = 1
+        if self.lookup(par_pos[0]) == self.lookup(par_pos[1]):
+            self.store(par_pos[2], 1)
         else:
-            self.code[par_pos[2]] = 0
+            self.store(par_pos[2], 0)
         self.pos += 4
+        return True
+
+    # 9
+    def relative_base_offset(self,mode):
+        np = 1
+        par_pos = self.interpret_parameters(np,mode)
+        self.relative_base += self.lookup(par_pos[0])
+        self.pos += (np + 1)
         return True
 
     # 99
@@ -100,6 +133,7 @@ class Intcode:
         self.code = code
         self.pos = pos
         self.terminated = False
+        self.relative_base = 0
         # A dictionary of operations for opcodes
         self.op_dict = {
             1: self.add,
@@ -110,6 +144,7 @@ class Intcode:
             6: self.jump_if_false,
             7: self.less_than,
             8: self.equals,
+            9: self.relative_base_offset,
             99: self.terminate,
         }
 
@@ -125,8 +160,9 @@ class Intcode:
         self.terminated = False
         while not self.terminated:
             if debug: print(self.code)
-            opcode = self.code[self.pos]
-            if debug: print(opcode)
+            opcode = self.lookup(self.pos)
+            if debug: print('pos:', self.pos, ' opcode:', opcode, ' rel_base:', self.relative_base)
             op = self.op_dict[opcode%100]
             mode = opcode//100
             op(mode)
+
