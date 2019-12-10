cde718c2e985472f04ecc3c52bcff582293b1de4
s-tefan <40999112+s-tefan@users.noreply.github.com>
2019-12-10 17:51:26 +0100

    intcode v2 (implements day 5)

--- aoc_2019_intcode.py
+++ aoc_2019_intcode.py
@@ -2,6 +2,7 @@ class Intcode:
  
 # operations for the different opcodes
 
+    # 1
     def add(self, mode=0):
         if mode == 0:
             arg1pos = self.code[self.pos+1]
@@ -18,6 +19,7 @@ class Intcode:
         self.pos += 4
         return True
 
+    # 2
     def mult(self, mode=0):
         if mode == 0:
             arg1pos = self.code[self.pos+1]
@@ -34,20 +36,48 @@ class Intcode:
         self.pos += 4
         return True
 
-    def terminate(self,mode=0):
-        self.terminated = True
+    # 3
+    def input(self, mode=0):
+        if mode == 0:
+            resultpos = self.code[self.pos+1]
+        elif mode == 1:
+            resultpos = self.pos+1
+        elif mode == 2:
+            pass
+        self.code[resultpos] = \
+            int(input('>'))
+        self.pos += 2
+        return True
+
+    # 3
+    def output(self, mode=0):
+        if mode == 0:
+            argpos = self.code[self.pos+1]
+        elif mode == 1:
+            argpos = self.pos+1
+        elif mode == 2:
+            pass
+        print(self.code[argpos])
+        self.pos += 2
         return True
 
 
+    # 99
+    def terminate(self,mode=0):
+        self.terminated = True
+        return True
 
 
     def __init__(self, code = [], pos = 0):
         self.code = code
         self.pos = pos
         self.terminated = False
-        self.op_dir = {
+        # A dictionary of operations for opcodes
+        self.op_dict = {
             1: self.add,
             2: self.mult,
+            3: self.input,
+            4: self.output,
             99: self.terminate,
         }
 
@@ -66,7 +96,7 @@ class Intcode:
             print(self.code)
             opcode = self.code[self.pos]
             print(opcode)
-            op = self.op_dir[opcode%100]
+            op = self.op_dict[opcode%100]
             mode = opcode//100
             op(mode)
             
@@ -76,7 +106,13 @@ class Intcode:
  
 
 
-test_code = [1,9,10,3,2,3,11,0,99,30,40,50]
-test = Intcode(test_code, 0)
-test.run()
-print(test.code)
+test_code2 = [1,9,10,3,2,3,11,0,99,30,40,50]
+test2 = Intcode(test_code2, 0)
+test2.run()
+print(test2.code)
+
+test5 = Intcode([3,0,4,0,99],0)
+test5.run()
+test5.run()
+print(test5.code)
+
